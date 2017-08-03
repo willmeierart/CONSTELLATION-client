b201bc4b58cad3b648e5e5901d7a8021b979f6af
Chase Hemerda <chasehemerda@icloud.com>
2017-08-03 09:30:05 -0600

    changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppProvider from './redux-config/AppProvider'
+import './styles/main.css'
 
 ReactDOM.render( <AppProvider/> , document.getElementById('root'))
